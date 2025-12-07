401ff0f722d8e757cf898319d52ec12be56798f9
Massimo Pavoni <maspavoni@gmail.com>
2025-12-07 14:11:07 +0100

    Change tachyon particle timelines solution to just cycle rows in reverse and sum timelines

--- src/laboratories.rs
+++ src/laboratories.rs
@@ -1,10 +1,4 @@
-use grid::Grid;
-use rustc_hash::FxHashMap;
-
-use crate::random_utils::{
-    FxHashWithCapacity, bytes_grid,
-    pos::{GridPosGet, Pos},
-};
+use crate::random_utils::bytes_grid;
 
 // ------------------------------------------------------------------------------------------------
 // Exports
@@ -36,47 +30,20 @@ pub fn tachyon_beam_splits(input: &str) -> u16 {
 }
 
 pub fn tachyon_particle_timelines(input: &str) -> u64 {
-    const LEFT: Pos = Pos::new(2, -1);
-    const RIGHT: Pos = Pos::new(2, 1);
-    const DOWN: Pos = Pos::new(2, 0);
+    let grid = bytes_grid(input);
+    let mut timelines = vec![1; grid.cols()];
 
-    // Use memoization to count all possible timelines
-    fn follow_particle(
-        grid: &Grid<u8>,
-        seen: &mut FxHashMap<Pos, u64>,
-        position: Pos,
-        timelines: u64,
-    ) -> u64 {
-        if let Some(count) = seen.get(&position) {
-            return timelines + count;
-        }
-
-        if let Some(&byte) = grid.pos_get(position) {
-            let count = if byte == b'^' {
-                follow_particle(grid, seen, position + LEFT, timelines)
-                    + follow_particle(grid, seen, position + RIGHT, timelines)
-            } else {
-                follow_particle(grid, seen, position + DOWN, timelines)
-            };
-
-            seen.insert(position, count);
-            count
-        } else {
-            timelines + 1
+    // Sum all possible timelines in reverse
+    for row in grid.iter_rows().skip(1).rev().skip(1).step_by(2) {
+        for (y, &byte) in row.enumerate() {
+            if byte == b'^' {
+                timelines[y] = timelines[y - 1] + timelines[y + 1];
+            }
         }
     }
 
-    let grid = bytes_grid(input);
-
-    follow_particle(
-        &grid,
-        &mut FxHashMap::with_capacity(64),
-        Pos::from((
-            2,
-            grid.iter_row(0)
-                .position(|&b| b == b'S')
-                .expect("Expected beam entrance"),
-        )),
-        0,
-    )
+    timelines[grid
+        .iter_row(0)
+        .position(|&b| b == b'S')
+        .expect("Expected beam entrance")]
 }
